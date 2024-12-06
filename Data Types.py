cd7c6f6270e8745431f426e57491fb1d80baa043
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-06 15:52:04 +0500

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,21 @@
+Name = "Abdulrehman"
+Age = 10
+Weight = 39.5
+Is_Student = True
+
+print("Name :", Name)
+print("Data Type of Name is" , type(Name))
+print("Age :", Age)
+print("Data Type of Age is" , type(Age))
+print("Weight :", Weight)
+print("Data Type of Weight is" , type(Weight))
+print("Is_Student :", Is_Student)
+print("Data Type of Is_Student is" , type(Is_Student))
+
+print("\n After Type Casting......" )
+Age = str(Age)
+print("Age")
+print("Data Type of Age is" , type(Age))
+Weight = int(Weight)
+print("Weight")
+print("Data Type of Age is" , type(Weight))
