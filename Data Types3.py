52fa17f2cbe7af09379efb6871c48bd782b7bdf8
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-08 09:02:37 +0500

    Add files via upload

---
+++ Data Types3.py
@@ -0,0 +1,15 @@
+first_name = "Codingal"
+last_name = "Educations"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String multiplied by 5 gives this result:", example)
+
+word ="Coding"
+print("Legnth of a string:", len(word))
+print("First letter of the word:", word[0])
+print("First letter of the word:", word[5])
+print("String Sliced:", word[0:3])
