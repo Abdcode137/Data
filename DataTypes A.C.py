350ae6701aa56c8a47bac7d517f41c1a1aa435d2
Abdcode137 <abdulrehman.qamer123@gmail.com>
2024-12-11 18:26:10 +0500

    Add files via upload

---
+++ DataTypes A.C.py
@@ -0,0 +1,11 @@
+name = "Abdulrehman Qamer"
+age = "10"
+grade = "Fifth"
+
+print("Name:", name)
+print("Age:", age)
+print("Grade:", grade)
+
+print("All strings concatenated")
+
+print(name" " + age" " + grade" ")
